6e56d61666a47c20cfb5085317ee012b95e1e854
ritesh-ghiya-cs <149673688+ritesh-ghiya-cs@users.noreply.github.com>
2024-10-09 12:15:09 +0530

    [CD-5928] | Merging changes of all these stories : CD-2180|CD-2159|CD-1913|CD-1906|CD-5475|ArtifactoryChanges (#7)

--- plugin-api/src/main/java/org/sonar/api/issue/DefaultTransitions.java
+++ plugin-api/src/main/java/org/sonar/api/issue/DefaultTransitions.java
@@ -90,6 +90,8 @@ public interface DefaultTransitions {
    */
   String RESET_AS_TO_REVIEW = "resetastoreview";
 
+  String RESOLVE_AS_EXCEPTION = "resolveasexception";
+
   /**
    * @since 4.4
    */
